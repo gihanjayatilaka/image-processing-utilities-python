b134ea6d23109821e8f8de948b21cf217fd0192d
Gihan Chanaka Jayatilaka <gihanjayatilaka@eng.pdn.ac.lk>
2020-06-26 10:32:07 +0530

    Update image-enhancement-metrics.py

--- image-enhancement-metrics.py
+++ image-enhancement-metrics.py
@@ -3,6 +3,15 @@
     pip install scikit-image
 '''
 
+
+'''
+python image-enhancement-metrics.py -i mercon-results/gt/ -o mercon-results/dark/
+python image-enhancement-metrics.py -i mercon-results/gt/ -o mercon-results/gt/
+python image-enhancement-metrics.py -i mercon-results/gt/ -o mercon-results/dcgan/
+python image-enhancement-metrics.py -i mercon-results/gt/ -o mercon-results/retinex/
+python image-enhancement-metrics.py -i mercon-results/gt/ -o mercon-results/our/
+'''
+
 def aa(x):
     return round(x*10000)/10000
 
